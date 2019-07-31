525890cdfa0f50d0fc2652ae40eaf981938a6f62
hynra <hynra@yahoo.com>
2019-07-31 08:06:36 +0700

    define roles

---
+++ src/roles.js
@@ -0,0 +1,15 @@
+module.exports = {
+    USER: "user",
+    PUBLIC: "public",
+    STUDENT : "student",
+    INSTRUCTOR: "instructor",
+    ORGANIZATION: "organization",
+    ORGANIZATION_ADMINISTRATOR: "oranization-administrator",
+    ORGANIZATION_DEPARTMENT_HEAD: "oranization-department-head",
+    ORGANIZATION_FINANCE_ADMIN: "organization-finance-admin",
+    ORGANIZATION_HR_ADMIN: "organization-hr-admin",
+    ORGANIZATION_HR_ASSISTANT: "organization-hr-assistant",
+    ORGANIZATION_PAYROLL_MASTER: "organization-payroll-master",
+    ORGANIZATION_STAFF: "organization-Staff",
+    ORGANIZATION_SUPERVISOR: "organization-supervisor"
+};
